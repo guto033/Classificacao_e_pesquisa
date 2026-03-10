a3b266de347846aab7926f942cb923c35c60065d
AugustoPasetto <148265090+guto033@users.noreply.github.com>
2026-03-09 21:47:07 -0300

    Create lista_ordenada.py

---
+++ 09.03/lista_ordenada.py
@@ -0,0 +1,58 @@
+import random
+import time
+
+array = []
+random.seed(2026)
+array = [random.randint(10,50) for _ in range(10000)]
+
+
+def busca_num():
+    numero = int(input("\nDigite um número: "))
+    return numero
+
+def busca(array, numero):
+    for i in range(len(array)):
+        if array[i] == numero:
+            print("O número está na lista\n")
+            return
+    print("O número não está na lista")
+
+def ordenar_lista(array):
+    start_time = time.time()
+    tam = len(array)
+    for j in range(tam-1):
+        maior = j
+        for i in range(j+1, tam):
+            if array[i] < array[maior]:
+                maior = i
+        array[j], array[maior] = array[maior], array[j]
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return elapsed_time
+
+def transferencia(array):
+    start_time = time.time()
+    tam = len(array)
+    array2 = []
+    maior = 0
+    
+    for i in range(len(array)):
+        if array[i] > maior:
+            array2.append(array[i])
+            maior = array[i]
+    end_time = time.time()
+    elapsed_time = end_time - start_time        
+    return elapsed_time
+
+
+    
+#numero = busca_num()
+#busca(array, numero)
+
+ordenacao = ordenar_lista(array)
+print(ordenacao)
+
+transferir = transferencia(array)
+##array.clear()
+##print(array)
+print(transferir)
